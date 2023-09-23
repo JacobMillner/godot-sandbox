9d51e0563ea32a4a976c580cfbdc9f9aafd0afe7
Jacob Millner <jacob.millner@outlook.com>
2023-09-23 17:43:29 -0400

    more progress on 3d platformer

--- 3dPlatformer/Scripts/Player.gd
+++ 3dPlatformer/Scripts/Player.gd
@@ -6,18 +6,40 @@ var gravity: float = 20.0
 
 var facing_angle: float
 
+var score: int
+
 @onready var model: MeshInstance3D = get_node("Model")
+@onready var score_text : Label = get_node("ScoreText")
 
 func _physics_process(delta):
+	# apply gravity if we're in the air
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	# get keyboard input
 	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	# calc move direction
 	var dir = Vector3(input.x, 0, input.y)
 	
+	# assign direction to velocity
 	velocity.x = dir.x * move_speed
 	velocity.z = dir.z * move_speed
 	
 	move_and_slide()
+	
+	# if we are moving, change model angle
 	if input.length() > 0:
 		facing_angle = Vector2(input.y, input.x).angle()
 	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5)
+	
+	if global_position.y < -5:
+		game_over()
+	
+func game_over():
+	get_tree().reload_current_scene()
+
+func add_score(amount):
+	score += amount
+	score_text.text = str("Score: ", score)
