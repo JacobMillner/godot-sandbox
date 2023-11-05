a765eedd1bf931b53fe23bf4956a0bcf781b4f3e
Jacob Millner <jacob.millner@outlook.com>
2023-11-05 17:42:25 -0500

    move camera with head bob

--- HorrorGame/Player/player.gd
+++ HorrorGame/Player/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody3D
 
 @onready var camera_3d = $Camera3D
+@onready var origCamPos : Vector3 = camera_3d.position
 var mouse_sense := 0.15
 var direction
 var speed := 4
 var jump := 30.0
 const GRAVITY := 5
+
+var _delta := 0.0
+var camBobSpeed := 10
+var camBobUpDown := 1
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,6 +23,10 @@ func _input(event):
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 func _process(delta):
+	process_camBob(delta)
+	
+
+func _physics_process(delta):
 	process_movement(delta)
 
 func process_movement(delta):
@@ -42,3 +51,17 @@ func process_movement(delta):
 		velocity.y -= GRAVITY
 
 	move_and_slide()
+
+func process_camBob(delta):
+	_delta += delta
+	
+	var cam_bob
+	var objCam
+	if direction != Vector3.ZERO:
+		cam_bob = floor(abs(direction.z) + abs(direction.x)) * _delta * camBobSpeed
+		objCam = origCamPos + Vector3.UP * sin(cam_bob) * camBobUpDown
+	else:
+		cam_bob = floor(abs(1) + abs(1)) * _delta * 0.5
+		objCam = origCamPos + Vector3.UP * sin(cam_bob) * camBobUpDown * 0.1
+	
+	camera_3d.position = camera_3d.position.lerp(objCam, delta)
