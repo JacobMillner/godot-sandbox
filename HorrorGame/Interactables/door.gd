a1a9eeded126e50a57dd90b2043d2ae4c028927e
Jacob Millner <jacob.millner@outlook.com>
2023-12-15 21:27:58 -0500

    door locked

--- HorrorGame/Interactables/door.gd
+++ HorrorGame/Interactables/door.gd
@@ -5,16 +5,37 @@ extends Interactable
 var is_open := false
 var can_interact := true
 
+@export var is_locked := false
+@export_node_path("Area3D") var key_path
+var actual_key
+
+func _ready() -> void:
+	if key_path != null:
+		actual_key = get_node(key_path)
+
 func action_use():
-	if is_open:
-		close()
+	if is_locked and !is_instance_valid(actual_key):
+		is_locked = false
+		
+	if !is_locked:
+		if can_interact:
+			if is_open:
+				close()
+			else:
+				open()
 	else:
-		open()
+		animation_player.play("door_locked")
 
 func close():
+	can_interact = false
 	is_open = false
 	animation_player.play("door_close")
 	
 func open():
+	can_interact = false
 	is_open = true
 	animation_player.play("door_open")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	can_interact = true
